336c84d62626474abbe558b8b7dcb34aa98160d6
Liron Tyomkin <liron.tyomkin@gmail.com>
2020-08-20 14:40:30 +0300

    started Logistic Regression

--- Simple_models.py
+++ Simple_models.py
@@ -1,11 +1,26 @@
 import Data_preprocessing as dp
 from sklearn.linear_model import LogisticRegression
-
-if __name__ == '__main__':
-    pass
-
+import numpy as np
+from sklearn.preprocessing import StandardScaler
 
 def logistic_regression():
-    train, test = dp.prepare_train_test()
+    test_set_x, test_set_y, train_set_x, train_set_y = dp.prepare_train_test()
 
-    lr = LogisticRegression()
+    scaler = StandardScaler()
+    scaler.fit(train_set_x)
+
+    scaled_train_x = scaler.transform(train_set_x)
+    scaled_test_x = scaler.transform(test_set_x)
+
+    lr = LogisticRegression(max_iter=5000)
+    lr.fit(scaled_train_x, train_set_y)
+
+    predictions = lr.predict(scaled_test_x)
+    acc = np.average([pred==real for pred, real in zip(predictions, test_set_y)])
+
+    print(acc)
+
+
+if __name__ == '__main__':
+    logistic_regression()
+
