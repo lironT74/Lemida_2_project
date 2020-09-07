3b0f960d4ab40ed89e90f402103a3a086db37927
Hadar <hadarsi320@gmail.com>
2020-09-07 11:49:45 +0300

    Changed the basic models such that they receive the data instead of creating it themselves

--- Simple_models.py
+++ Simple_models.py
@@ -10,14 +10,15 @@ from sklearn.tree import export_graphviz
 # import graphviz
 from graphviz import Source
 import pydot
+import matplotlib.pyplot as plt
 
 
 def calc_acc(predictions, real):
     return np.average(predictions == real)
 
 
-def one_vs_all(model, name='model'):
-    test_set_x, test_set_y, train_set_x, train_set_y = dp.prepare_train_test()
+def one_vs_all(model, data, name='model'):
+    test_set_x, test_set_y, train_set_x, train_set_y = data
 
     ovr = OneVsRestClassifier(model)
     ovr.fit(train_set_x, train_set_y)
@@ -28,33 +29,32 @@ def one_vs_all(model, name='model'):
     print(f'{f"One vs All {name} accuracy:":50} {round(acc, 3)}')
 
 
-def decision_tree(max_depth=None, scale=True):
-    test_set_x, test_set_y, train_set_x, train_set_y = dp.prepare_train_test(scale)
+def decision_tree(data, max_depth=None, save_tree=True):
+    test_set_x, test_set_y, train_set_x, train_set_y = data
     data_columns = dp.X_COLUMNS
 
     dt = DecisionTreeClassifier(max_depth=max_depth)
     dt.fit(train_set_x, train_set_y)
 
-    export_graphviz(dt, out_file=f"./tree_graphs/tree.dot",
-                    feature_names=data_columns,
-                    class_names=["low", "medium", "high"],
-                    rounded=True, proportion=False,
-                    precision=2, filled=True)
+    if save_tree:
+        export_graphviz(dt, out_file=f"./tree_graphs/tree.dot",
+                        feature_names=data_columns,
+                        class_names=["low", "medium", "high"],
+                        rounded=True, proportion=False,
+                        precision=2, filled=True)
 
-    Source.from_file(f"./tree_graphs/tree.dot")
-
-    (graph,) = pydot.graph_from_dot_file(f"./tree_graphs/tree.dot")
-
-    graph.write_png(f'./tree_graphs/tree_{dt.tree_.max_depth}_.png')
+        Source.from_file(f"./tree_graphs/tree.dot")
+        (graph,) = pydot.graph_from_dot_file(f"./tree_graphs/tree.dot")
+        graph.write_png(f'./tree_graphs/tree_{dt.tree_.max_depth}_.png')
 
     predictions = dt.predict(test_set_x)
     acc = calc_acc(predictions, test_set_y)
-    print(f'{"Decision Tree max depth:":50} {dt.tree_.max_depth}')
+    print(f'{"Decision Tree max depth:":50}  {dt.tree_.max_depth}')
     print(f'{"Decision Tree accuracy:":50} {round(acc, 3)}')
 
 
-def logistic_regression():
-    test_set_x, test_set_y, train_set_x, train_set_y = dp.prepare_train_test()
+def logistic_regression(data):
+    test_set_x, test_set_y, train_set_x, train_set_y = data
 
     lr = LogisticRegression(solver='lbfgs', multi_class='auto')
     lr.fit(train_set_x, train_set_y)
@@ -64,8 +64,8 @@ def logistic_regression():
     print(f'{"Logistic Regression accuracy:":50} {round(acc, 3)}')
 
 
-def naive_bayes():
-    test_set_x, test_set_y, train_set_x, train_set_y = dp.prepare_train_test()
+def naive_bayes(data):
+    test_set_x, test_set_y, train_set_x, train_set_y = data
 
     nb = GaussianNB()
     nb.fit(train_set_x, train_set_y)
@@ -78,13 +78,11 @@ def naive_bayes():
 if __name__ == '__main__':
     # logistic_regression()
     # naive_bayes()
-    for i in range(1, 25):
-        decision_tree(max_depth=i, scale=False)
-    decision_tree(scale=False)
+    # decision_tree(max_depth=5, scale=False, save_tree=False)
+    # decision_tree(scale=False, save_tree=False)
     #
-    # one_vs_all(Perceptron(max_iter=5000), 'perceptron')
-    # one_vs_all(LogisticRegression(max_iter=5000, solver='lbfgs'), 'logistic regression')
-    # one_vs_all(LinearSVC(max_iter=5000), 'svm')
+    # one_vs_all(Perceptron(max_iter=500000), 'perceptron')
+    # one_vs_all(LogisticRegression(max_iter=500000, solver='lbfgs'), 'logistic regression')
     #
     # one_vs_all(DecisionTreeClassifier(), 'decision tree')
     #
@@ -92,3 +90,18 @@ if __name__ == '__main__':
     # one_vs_all(SVC(max_iter=500000, kernel="rbf"), 'rbf svm')
     # one_vs_all(SVC(max_iter=500000, kernel="poly"), 'poly svm')
     # one_vs_all(SVC(max_iter=500000, kernel="sigmoid"), 'sigmoid svm')
+
+    data = dp.prepare_train_test(scale=False, categorized=False)
+    acc = []
+    for i in range(1, 25):
+        acc.append(decision_tree(data, max_depth=i))
+
+    print(np.argmax(acc))
+
+    plt.title("DesicionTree_X test accuracy")
+    plt.plot(range(1, len(acc) + 1), acc, color="fuchsia")
+    plt.xlabel("X")
+    plt.xticks(range(1, len(acc) + 1))
+    plt.ylabel("test accuracy", rotation=90)
+    plt.show()
+
