1653a2d96df0ac8f29c3276f5ad85bcd327e0497
Liron Tyomkin <liron.tyomkin@gmail.com>
2020-09-19 18:36:43 +0300

    now splitting data for simple models not by days

--- Unstructured_models.py
+++ Unstructured_models.py
@@ -1,4 +1,4 @@
-import data_preprocessing as dp
+from data_preprocessing import *
 from sklearn.linear_model import LogisticRegression
 import numpy as np
 from sklearn.multiclass import OneVsRestClassifier
@@ -31,7 +31,7 @@ def one_vs_all(model, data, name='model'):
 
 def decision_tree(data, max_depth=None, save_tree=False):
     test_set_x, test_set_y, train_set_x, train_set_y = data
-    data_columns = dp.X_COLUMNS
+    data_columns = X_COLUMNS
 
     dt = DecisionTreeClassifier(max_depth=max_depth)
     dt.fit(train_set_x, train_set_y)
@@ -76,8 +76,7 @@ def naive_bayes(data):
     print(f'{"Naive Bayes accuracy:":50} {round(acc, 3)}')
 
 
-def decision_tree_graph():
-    data = dp.prepare_train_test(scale=False, categorized=False)
+def decision_tree_graph(data):
     acc = []
     for i in range(1, 25):
         acc.append(decision_tree(data, max_depth=i))
@@ -94,21 +93,25 @@ def decision_tree_graph():
     plt.show()
 
 if __name__ == '__main__':
-    pass
-    # logistic_regression()
-    # naive_bayes()
-    # decision_tree(max_depth=5, scale=False, save_tree=False)
-    # decision_tree(scale=False, save_tree=False)
-    #
-    # one_vs_all(Perceptron(max_iter=500000), 'perceptron')
-    # one_vs_all(LogisticRegression(max_iter=500000, solver='lbfgs'), 'logistic regression')
-    #
-    # one_vs_all(DecisionTreeClassifier(), 'decision tree')
-    #
-    # one_vs_all(SVC(max_iter=500000, kernel="linear"), 'linear svm')
-    # one_vs_all(SVC(max_iter=500000, kernel="rbf"), 'rbf svm')
-    # one_vs_all(SVC(max_iter=500000, kernel="poly"), 'poly svm')
-    # one_vs_all(SVC(max_iter=500000, kernel="sigmoid"), 'sigmoid svm')
+
+    data = prepare_train_test()
+
+    logistic_regression(data)
+    naive_bayes(data)
+    decision_tree(data, max_depth=5, save_tree=False)
+    decision_tree(data, save_tree=False)
+
+    one_vs_all(Perceptron(max_iter=500000), data, 'perceptron')
+    one_vs_all(LogisticRegression(max_iter=500000, solver='lbfgs'),data,  'logistic regression')
+
+    one_vs_all(DecisionTreeClassifier(), data, 'decision tree')
+
+    one_vs_all(SVC(max_iter=500000, kernel="linear"),data, 'linear svm')
+    one_vs_all(SVC(max_iter=500000, kernel="rbf"),data,  'rbf svm')
+    one_vs_all(SVC(max_iter=500000, kernel="poly"), data, 'poly svm')
+    one_vs_all(SVC(max_iter=500000, kernel="sigmoid"),data,  'sigmoid svm')
+
+    decision_tree_graph(data)
 
 
 
